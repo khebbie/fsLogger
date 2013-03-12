9fb3f8e9dcdd7c1bf96ea319123f4d308e680346
Klaus Hebsgaard <klaus@hebsgaard.dk>
2013-03-12 10:19:59 +0100

    added logtester console app

---
+++ src/logTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using fsLogger;
+
+namespace logTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger1 = LoggerManager.GetLogger("test");
+            logger1.Log(LogMessage.NewDebug("test"));
+            logger1.Log(LogMessage.NewError("big error", new Exception("tet")));
+        }
+    }
+}
